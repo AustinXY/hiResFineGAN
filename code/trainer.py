574f0897181e89e06b12736365be69fd2300dcd7
austin <ayxue@ucdavis.edu>
2020-08-09 23:17:59 -0700

    u

--- code/trainer.py
+++ code/trainer.py
@@ -254,12 +254,12 @@ class FineGAN_trainer(object):
                 y2 =  self.warped_bbox[3][i]
 
                 a1 = max(torch.tensor(0).float().cuda(), torch.ceil((x1 - self.recp_field)/self.patch_stride))
-                a2 = min(torch.tensor(self.n_out - 1).float().cuda(), torch.floor((self.n_out - 1) - ((126 - self.recp_field) - x2)/self.patch_stride)) + 1
+                a2 = min(torch.tensor(self.n_out - 1).float().cuda(), torch.floor((self.n_out - 1) - ((126 - self.recp_field) - x2)/self.patch_stride))
                 b1 = max(torch.tensor(0).float().cuda(), torch.ceil((y1 - self.recp_field)/self.patch_stride))
-                b2 = min(torch.tensor(self.n_out - 1).float().cuda(), torch.floor((self.n_out - 1) - ((126 - self.recp_field) - y2)/self.patch_stride)) + 1
+                b2 = min(torch.tensor(self.n_out - 1).float().cuda(), torch.floor((self.n_out - 1) - ((126 - self.recp_field) - y2)/self.patch_stride))
 
                 if (x1 != x2 and y1 != y2):
-                    weights_real[i, :, a1.type(torch.int) : a2.type(torch.int) , b1.type(torch.int) : b2.type(torch.int)] = 0.0
+                    weights_real[i, :, a1.type(torch.int) : a2.type(torch.int)+1, b1.type(torch.int) : b2.type(torch.int)+1] = 0.0
 
             norm_fact_real = weights_real.sum()
             norm_fact_fake = weights_real.shape[0]*weights_real.shape[1]*weights_real.shape[2]*weights_real.shape[3]
